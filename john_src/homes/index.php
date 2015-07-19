88d379eb427e4849a8ea08f6764e912b090bcfec
hkri <john_granturismo04@yahoo.com>
2015-07-20 02:09:14 +0800

    Updated index page of Alari Homes.

--- john_src/homes/index.php
+++ john_src/homes/index.php
@@ -22,6 +22,11 @@
 	</head>
 
 	<body>
+		<div id = 'fader' class = "fader"></div>
+		<!-- If script is enabled, make fading possible. -->
+		<script>
+			document.getElementById("fader").style.display= 'block';
+		</script>
 
 		<div class = 'header'>
 			<div class = 'wrapper'>
@@ -43,7 +48,12 @@
 		</div>
 
 		<div class = 'content'>
+			<!--
 			<h1 style = 'position: absolute;top: 50%; width: 100%;text-align: center;font-weight: normal;opacity: 0.7'>TODO: PUT "HOME/ABOUT US" RELATED STUFF HERE.</h1>
+			-->
+			<div class = 'cover-photo' style = 'background-image:url("../res/homes/homes_cover.jpg")'>
+				<h1 class = 'light'>About Us</h1>
+			</div>
 		</div>
 
 		<div class = 'footer'>
@@ -53,20 +63,21 @@
 		
 
 		<!-- jQuery and Javascript Friends -->
-		<script src="scripts/jquery-2.0.1.min.js"></script>
-		<script src="scripts/jquery.easing.1.3.js"></script>
-		<script src="scripts/jquery.scrollTo.min.js"></script>
+		<script src="../scripts/jquery-2.0.1.min.js"></script>
+		<script src="../scripts/jquery.easing.1.3.js"></script>
+		<script src="../scripts/jquery.scrollTo.min.js"></script>
+		<script src="../scripts/page-fader.js"></script>
 
 		<script type = 'text/javascript'>
 			//Add reference to jQuery and Easings library
 			var $root = $('body, html');
-			var $lnk = $('.scrolllink);
+			var $lnk = $('.scrolllink');
 			var $doc = $(document);
 			var idTo = '';
 
 			$doc.ready(function(){
 
-				//smooth scroll function.
+				//Enable smooth scroll.
 				$lnk.click(function(){
 					event.preventDefault();
 					idTo =  $(this).attr('href');
@@ -75,6 +86,8 @@
 				    }, 1000, 'easeInOutQuint');
 				    return false;
 				});
+
+				fadePage();
 
 			});
 
