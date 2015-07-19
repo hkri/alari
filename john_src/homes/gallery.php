4b910e1956aceb2c5c2422c120ac9c9190fc3bcf
hkri <john_granturismo04@yahoo.com>
2015-07-20 02:09:35 +0800

    Updated gallery page of Alari Homes.

--- john_src/homes/gallery.php
+++ john_src/homes/gallery.php
@@ -22,6 +22,11 @@
 	</head>
 
 	<body>
+		<div id = 'fader' class = "fader"></div>
+		<!-- If script is enabled, make fading possible. -->
+		<script>
+			document.getElementById("fader").style.display= 'block';
+		</script>
 
 		<div class = 'header'>
 			<div class = 'wrapper'>
@@ -43,7 +48,45 @@
 		</div>
 
 		<div class = 'content'>
-			<h1 style = 'position: absolute;top: 50%; width: 100%;text-align: center;font-weight: normal;opacity: 0.7'>TODO: PUT "GALLERY" RELATED STUFF HERE.</h1>
+			<div class = 'cover-photo' style = 'background-image:url("../res/homes/homes_gallery_cover.jpg")'>
+				<h1 class = 'light'>Gallery</h1>
+			</div>	
+
+			<div class = 'gallery'>
+				<div class = 'thumb-wrapper'>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p1.png' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p2.png' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p3.png' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p4.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p5.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p6.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p7.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p8.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p9.jpg' />
+					</div>
+					<div class = 'photothumb thumbnail-160'>
+						<img src = 'gallery/p10.jpg' />
+					</div>
+
+				</div>
+			</div>
 		</div>
 
 		<div class = 'footer'>
@@ -53,16 +96,18 @@
 		
 
 		<!-- jQuery and Javascript Friends -->
-		<script src="scripts/jquery-2.0.1.min.js"></script>
-		<script src="scripts/jquery.easing.1.3.js"></script>
-		<script src="scripts/jquery.scrollTo.min.js"></script>
+		<script src="../scripts/jquery-2.0.1.min.js"></script>
+		<script src="../scripts/jquery.easing.1.3.js"></script>
+		<script src="../scripts/jquery.scrollTo.min.js"></script>
+		<script src="../scripts/page-fader.js"></script>
 
 		<script type = 'text/javascript'>
 			//Add reference to jQuery and Easings library
 			var $root = $('body, html');
-			var $lnk = $('.scrolllink);
+			var $lnk = $('.scrolllink');
 			var $doc = $(document);
 			var idTo = '';
+			var $imgthumb = $('.photothumb img');
 
 			$doc.ready(function(){
 
@@ -74,6 +119,29 @@
 				        scrollTop: $(idTo).offset().top
 				    }, 1000, 'easeInOutQuint');
 				    return false;
+				});
+
+				fadePage();
+
+				//Make image fit in wrapping div.
+				$imgthumb.each(function(key, value){
+					var $tb = $(value);
+					var tw = parseInt($tb.css('width'));
+					var th = parseInt($tb.css('height'));
+					var thumbsize = parseInt($tb.parent().css('height'));
+					if(tw > th){
+						var factor = thumbsize / th;
+						th *= factor;
+						tw *= factor;
+						$tb.css('height',  th);
+						$tb.css('width', tw);
+					} else{
+						var factor = thumbsize / tw;
+						th *= factor;
+						tw *= factor;
+						$tb.css('height',  th);
+						$tb.css('width', tw);
+					}
 				});
 
 			});
