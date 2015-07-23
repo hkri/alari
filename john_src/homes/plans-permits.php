ef45f763266b0413d2afa162f14a076add95581d
hkri <john_granturismo04@yahoo.com>
2015-07-24 00:30:59 +0800

    Designed the Plans and Permits pages of Alari Homes.

--- john_src/homes/plans-permits.php
+++ john_src/homes/plans-permits.php
@@ -51,6 +51,229 @@
 			<div class = 'cover-photo' style = 'background-image:url("../res/homes/homes_plans.jpg")'>
 				<h1 class = 'light'>Plans and Permits</h1>
 			</div>
+
+			<div class = 'toolbar'>
+				<div class = 'option selected'>
+					<div class = 'shadow'></div>
+					<a class = ''>Plans</a>
+				</div>
+				<div class = 'option'>
+					<div class = 'shadow'></div>
+					<a class = 'option-text' href = '#'>Permits</a>
+				</div>
+			</div>
+			<div class = 'listbox'>
+
+				<!-- Show when no items available --> <!--
+				<h1 class = 'no-items'>Sorry, no items available.</h1>
+				-->
+
+				<!-- Section Title in case separation required -->
+				<h1 class = 'section-title'>Main House Plans</h1>
+
+				<!-- Listbox Item Plans/Permit/Shop Drawings -->
+
+				<div class = 'listitem'>
+					<h1>A000</h1> <!-- Plan title -->
+					<h6>Blake Yoon</h6> <!-- Plan Author/Architect -->
+					<hr />
+					<div class = 'description'>	 <!-- Plan Description -->
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					 <!-- To do: updated href to plan PDF link -->
+					<a target = '_blank' href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AACS3creaJrAb8Ui5-7QpEXza/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A000.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A004</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AACbunzMrHsSrtNyGRiso4YCa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A004.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A007</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AABLYB5DeBydrbAUMXmZCp03a/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A007.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A008</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AADbcM469BKHsP4e8ZgwMl3Ba/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A008.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A009</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAColMn_SO9TAlKm07p-GWsna/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A009.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A011</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AABDf01thzOZaAB0FZhly31Ha/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A011.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A014</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AABZ8Izk6l9Hn_afcsF6d6Waa/Ryan%27s%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/A014.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>S002</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Main House Renovation</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAACh_q4DqYT4nKhATBPIZIda/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/MAIN%20HOUSE/S002.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+
+				<h1 class = 'section-title'>Accessory House</h1>
+
+				<div class = 'listitem'>
+					<h1>A-2</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AACV_2FRNnfcOLsENUSXfOWsa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/A-2.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A-3</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAAn-NjzymC_LpzOJukg2vjCa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/A-3.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A-4</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAAZ8Es3Royk64tsl2_UTlrJa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/A-4.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>A-6</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AABFKq7toSzzj5Ld7H2xF4mna/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/A-6.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>S-1</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAAd5ISSt4HWg7d-0tXLncLNa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/S-1.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+
+				<div class = 'listitem'>
+					<h1>S-2</h1>
+					<h6>Blake Yoon</h6>
+					<hr />
+					<div class = 'description'>
+						<p>
+							<i>Proposed Garage and 2nd Flr.</i>
+							<br />
+							4079 Kulamanu St., Honolulu, Hawaii 96816
+						</p>
+					</div>
+					<a href = "https://www.dropbox.com/sh/hcmwz76gilgrfhz/AAAJLz78q5VRDxDm0lLBFxNpa/Ryan's%20File%20_for%20Website/PLANS%20-%20ALARI%20HOMES/ACCESSORY%20HOUSE/S-2.pdf?dl=0"><img src = '../res/icons/download.png' /><span>Download PDF</span></a>
+				</div>
+				
+			</div>
 		</div>
 
 		<div class = 'footer'>
@@ -60,7 +283,7 @@
 		
 
 		<!-- jQuery and Javascript Friends -->
-		<script src="../scripts/jquery-2.0.1.min.js"></script>
+		<script src="../scripts/jquery-1.8.0.min.js"></script>
 		<script src="../scripts/jquery.easing.1.3.js"></script>
 		<script src="../scripts/page-fader.js"></script>
 		<script src="../scripts/jquery.scrollTo.min.js"></script>
@@ -71,6 +294,7 @@
 			var $lnk = $('.scrolllink');
 			var $doc = $(document);
 			var idTo = '';
+
 
 			//Fade in when all page finishes loading.
 			$(window).bind("load", function() {
@@ -87,6 +311,14 @@
 				        scrollTop: $(idTo).offset().top
 				    }, 1000, 'easeInOutQuint');
 				    return false;
+				});
+
+				//check if overflow text in each thumbnail.
+				var $itemdesc = $('.listitem p');
+				$itemdesc.each(function(index, value){
+					if(value.scrollHeight > $(value).innerHeight()){
+						$(value).addClass('ellipsis');
+					}
 				});
 
 			});
