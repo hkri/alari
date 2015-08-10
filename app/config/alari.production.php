a186605c92a48a95c5d102f9cb9074da23629cd2
Ramil Espiritu <renegade.cipher@gmail.com>
2015-08-10 20:52:59 +0800

    Create production config for alari urls

---
+++ app/config/alari.production.php
@@ -0,0 +1,8 @@
+<?php
+define('OWNER_EMAIL', 'blakeyoon@yahoo.com'); // Owner's receiving email address for inquiries
+
+/**
+ * Youtube Video URLs
+ */
+define('VIDEO_URL_1', 'https://youtu.be/-KOmoyIHIzg'); // Video URL 1
+define('VIDEO_URL_2', 'https://youtu.be/4pojPMvxdoE'); // Video URL 2
