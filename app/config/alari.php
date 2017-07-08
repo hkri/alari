762f7e76b06a7503f3ba97029e3dab30edd36f6f
Ramil Nazareno E. Espiritu <renegade.cipher@gmail.com>
2017-07-08 11:24:10 +0800

    Remove cakephp files

--- app/config/alari.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('OWNER_EMAIL', 'spirit_renegade@yahoo.com'); // Owner's receiving email address for inquiries
-
-/**
- * Youtube Video URLs
- */
-define('VIDEO_URL_1', 'https://youtu.be/-KOmoyIHIzg'); // Video URL 1
-define('VIDEO_URL_2', 'https://youtu.be/4pojPMvxdoE'); // Video URL 2
