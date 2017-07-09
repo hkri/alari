ce0d84b76379eda89dd2e09c710a0591dfc891ae
hkri <john_granturismo04@yahoo.com>
2017-07-09 23:35:46 +0800

    Updated copyright year.

--- src/homes/dda/index.php
+++ src/homes/dda/index.php
@@ -1,120 +1,113 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<!-- title -->
-		<title>Alari Homes | DDA Services</title>
+  <head>
+    <!-- title -->
+    <title>Alari Homes | DDA Services</title>
 
-		<!-- Favicon -->
+    <!-- Favicon -->
 
-		<!-- Fonts -->
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
+    <!-- Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Karla:400,700' rel='stylesheet' type='text/css'>
 
-		<!-- External Styles -->
-		<link rel = 'stylesheet' href = '../../styles/reset.css' />
-		<link rel = 'stylesheet' href = '../../styles/alaristyle.css' />
+    <!-- External Styles -->
+    <link rel = 'stylesheet' href = '../../styles/reset.css' />
+    <link rel = 'stylesheet' href = '../../styles/alaristyle.css' />
 
-		<style type = 'text/css'>
+    <style type = 'text/css'>
 
-		</style>
+    </style>
 
-	</head>
+  </head>
 
-	<body>
-		<div class = 'header'>
-			<div class = 'wrapper'>
-				<div class = 'header-logobox noselect'>
-					<!-- Please use 380x80 pixel logo for best results. -->
-					<img class = 'nav-logo' src = '../../res/homes/homes-dummy-logo.png' />
-				</div>
-				<div class = 'header-navbox'>
-					<ul class = 'nav noselect'>
-						<a href = '/'><li>Home</li></a>
-						<a href = '../'><li>About Us</li></a>
-						<a class = 'active'><li>DDA Services</li></a>
-						<a href = '../our_team'><li>Our Team</li></a>
-						<a href = '../plans_permits'><li>Plans and Permits</li></a>
-						<a href = '../gallery'><li>Gallery</li></a>
-						<a href = '../contact'><li>Contact</li></a>
-						<a><li>4079 Kulamanu<ul class = 'dropdown'>
+  <body>
+    <div class = 'header'>
+      <div class = 'wrapper'>
+        <div class = 'header-logobox noselect'>
+          <!-- Please use 380x80 pixel logo for best results. -->
+          <img class = 'nav-logo' src = '../../res/homes/homes-dummy-logo.png' />
+        </div>
+        <div class = 'header-navbox'>
+          <ul class = 'nav noselect'>
+            <a href = '/'><li>Home</li></a>
+            <a href = '../'><li>About Us</li></a>
+            <a class = 'active'><li>DDA Services</li></a>
+            <a href = '../our_team'><li>Our Team</li></a>
+            <a href = '../plans_permits'><li>Plans and Permits</li></a>
+            <a href = '../gallery'><li>Gallery</li></a>
+            <a href = '../contact'><li>Contact</li></a>
+            <a><li>4079 Kulamanu<ul class = 'dropdown'>
               <li><a href = '../kulamanu_pricing'>Pricing</a></li>
               <li><a href = '../kulamanu_gallery'>Gallery</a></li>
             </ul></li></a>
-					</ul>
-				</div>
-			</div>
-		</div>
+          </ul>
+        </div>
+      </div>
+    </div>
 
-		<div class = 'content'>
-			<div class = 'cover-photo' style = 'background-image:url("../../res/homes/homes_cover.jpg")'>
-				<h1 class = 'light'>Drafting and Architectural Design Service</h1>
-			</div>
+    <div class = 'content'>
+      <div class = 'cover-photo' style = 'background-image:url("../../res/homes/homes_cover.jpg")'>
+        <h1 class = 'light'>Drafting and Architectural Design Service</h1>
+      </div>
 
-			<div class = 'size-fill'>
-					<h2 class = 'section-caption'>Here at ALARI Homes, we want you to be a part of our team - from your concept, hand in hand we can provide you</h2>
-			</div>
+      <div class = 'size-fill'>
+          <h2 class = 'section-caption'>Here at ALARI Homes, we want you to be a part of our team - from your concept, hand in hand we can provide you</h2>
+      </div>
 
-			<div class = 'wrapper dds'>
+      <div class = 'wrapper dds'>
 
-				<div class = 'row'>
-					<div class = 'cell size-25 icons'>
-						<img src = '../../res/homes/draft.png' alt = 'DDA Icon' />
-					</div>
-					<div class = 'cell size-fill service-item-text'>
-						<h2>Architectural Design Service (DDA Service)</h2>
-						<p>Ranging from conceptualization, permit and drawing phase to construction drawings.</p>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'cell size-25 icons'>
-						<img src = '../../res/homes/3d.png' alt = '3D Model Icon'  />
-					</div>
-					<div class = 'cell size-fill service-item-text'>
-						<h2>3D Visualization</h2>
-						<p>We can develop through conceptual sketch to 3D visualization.</p>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'cell size-25 icons'>
-						<img src = '../../res/homes/smodel.png' alt = 'Scaled Model Icon'  />
-					</div>
-					<div class = 'cell size-fill service-item-text'>
-						<h2>Scaled Modelling</h2>
-						<p>We can develop through conceptual sketch to 3D visualization.</p>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'cell size-25 icons'>
-						<img src = '../../res/homes/walkthrough.png' alt = 'Walkthrough Icon' />
-					</div>
-					<div class = 'cell size-fill service-item-text'>
-						<h2>Walkthrough</h2>
-						<p>We can even walk you through your home through our 3D walkthrough.</p>
-					</div>
-				</div>
-				<div class = 'row'>
-					<div class = 'cell size-25 icons'>
-						<img src = '../../res/homes/team.png' alt = 'Team Icon' />
-					</div>
-					<div class = 'cell size-fill service-item-text'>
-						<h2>Multi-disciplinary Team</h2>
-						<p>We also provide multi-disciplinary team to cater whole technical working package.</p>
-					</div>
-				</div>
+        <div class = 'row'>
+          <div class = 'cell size-25 icons'>
+            <img src = '../../res/homes/draft.png' alt = 'DDA Icon' />
+          </div>
+          <div class = 'cell size-fill service-item-text'>
+            <h2>Architectural Design Service (DDA Service)</h2>
+            <p>Ranging from conceptualization, permit and drawing phase to construction drawings.</p>
+          </div>
+        </div>
+        <div class = 'row'>
+          <div class = 'cell size-25 icons'>
+            <img src = '../../res/homes/3d.png' alt = '3D Model Icon'  />
+          </div>
+          <div class = 'cell size-fill service-item-text'>
+            <h2>3D Visualization</h2>
+            <p>We can develop through conceptual sketch to 3D visualization.</p>
+          </div>
+        </div>
+        <div class = 'row'>
+          <div class = 'cell size-25 icons'>
+            <img src = '../../res/homes/smodel.png' alt = 'Scaled Model Icon'  />
+          </div>
+          <div class = 'cell size-fill service-item-text'>
+            <h2>Scaled Modelling</h2>
+            <p>We can develop through conceptual sketch to 3D visualization.</p>
+          </div>
+        </div>
+        <div class = 'row'>
+          <div class = 'cell size-25 icons'>
+            <img src = '../../res/homes/walkthrough.png' alt = 'Walkthrough Icon' />
+          </div>
+          <div class = 'cell size-fill service-item-text'>
+            <h2>Walkthrough</h2>
+            <p>We can even walk you through your home through our 3D walkthrough.</p>
+          </div>
+        </div>
+        <div class = 'row'>
+          <div class = 'cell size-25 icons'>
+            <img src = '../../res/homes/team.png' alt = 'Team Icon' />
+          </div>
+          <div class = 'cell size-fill service-item-text'>
+            <h2>Multi-disciplinary Team</h2>
+            <p>We also provide multi-disciplinary team to cater whole technical working package.</p>
+          </div>
+        </div>
 
-			</div>
-
-		</div>
-
-		<div class = 'footer'>
-			<h6>&copy; 2015 Alari Homes and Fabrication. All Rights Reserved.</h6>
-		</div>
-
-
-
-		<!-- jQuery and Javascript Friends -->
-		<script src="../../scripts/jquery-1.8.0.min.js"></script>
-
-	</body>
+      </div>
+    </div>
+    <div class = 'footer'>
+      <h6>&copy; 2017 Alari Homes and Fabrication. All Rights Reserved.</h6>
+    </div>
+    <script src="../../scripts/jquery-1.8.0.min.js"></script>
+  </body>
 </html>
