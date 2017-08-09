e2f51926607a70aa06a40e7e6a04b5f21e190f5b
hkri <john.ioannishikari@gmail.com>
2017-08-09 13:34:25 -0400

    Added smooth scroller script.

---
+++ src/assets/js/smoothscroll.js
@@ -0,0 +1,25 @@
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          var $target = $(target);
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+          };
+        });
+      }
+    }
+  });
